4052b8bdebca82c317c50594f790f9fad39a463c
Renuja134 <ashanrenuja57@gmail.com>
2025-06-28 12:43:21 +0530

    Update functions.php

--- includes/functions.php
+++ includes/functions.php
@@ -1,1 +1,15 @@
+<?php
+function getTranslation($key) {
+    global $lang;
+    return $lang[$key] ?? $key;
+}
 
+function formatPrice($amount) {
+    $config = include '../config/settings.php';
+    return $config['currency'] . number_format($amount, 2);
+}
+
+function isAdmin() {
+    return $_SESSION['role'] === 'admin';
+}
+?>
