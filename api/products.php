811d1433dd881450c13214dbd57751ee25b275b8
Renuja134 <ashanrenuja57@gmail.com>
2025-06-28 18:46:02 +0530

    Update products.php

--- api/products.php
+++ api/products.php
@@ -13,7 +13,22 @@ switch($method){
         $stmt = $conn->query("SELECT * FROM products");
         echo json_encode($stmt->fetchAll(PDO::FETCH_ASSOC));
         break;
-        
+        <!-- භාණ්ඩ ලැයිස්තුවේ එක් එක් පේළිය තුළ -->
+<td id="quantity-<?= $product['id'] ?>">
+    <?= $product['quantity'] ?>
+</td>
+<td>
+    <button class="btn btn-sm btn-success btn-increase" 
+            data-id="<?= $product['id'] ?>" 
+            data-amount="5">
+        <i class="fas fa-plus"></i> 5
+    </button>
+    <button class="btn btn-sm btn-warning btn-decrease" 
+            data-id="<?= $product['id'] ?>" 
+            data-amount="1">
+        <i class="fas fa-minus"></i> 1
+    </button>
+</td>
     case 'POST':
         // Create new product
         $data = json_decode(file_get_contents('php://input'), true);
